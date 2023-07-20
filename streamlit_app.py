2faefc7eec657f16d143351e38bb8b7a9bdd567d
LoicChapron <98323891+LoicChapron@users.noreply.github.com>
2023-07-20 16:39:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,18 @@ st_profile_report(pr)
 """
 
 import streamlit as st
+import pandas as pd
 
-st.title('st.secrets')
+st.title('st.file_uploader')
 
-st.write(st.secrets['message'])
+st.subheader('Input CSV')
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+  df = pd.read_csv(uploaded_file)
+  st.subheader('DataFrame')
+  st.write(df)
+  st.subheader('Descriptive Statistics')
+  st.write(df.describe())
+else:
+  st.info('☝️ Upload a CSV file')
