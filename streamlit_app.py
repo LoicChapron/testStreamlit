7a20d958523d4ec49d01bc05e33b491b5eb4c20e
LoicChapron <98323891+LoicChapron@users.noreply.github.com>
2023-07-20 16:31:22 +0200

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,19 @@
+"""
+import streamlit as st
+import pandas as pd
+import pandas_profiling
+from streamlit_pandas_profiling import st_profile_report
+
+st.header('`streamlit_pandas_profiling`')
+
+df = pd.read_csv('testIstex.csv', delimiter="\t")
+df = df.drop(['publication_day', 'publication_month'], axis = 1)
+pr = df.profile_report()
+st_profile_report(pr)
+"""
+
+import streamlit as st
+
+st.title('st.secrets')
+
+st.write(st.secrets['message'])
